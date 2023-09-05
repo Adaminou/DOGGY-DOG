6ca9f62eedc3ad07c1869d43ba10b684691ef118
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2023-09-05 14:49:27 +0200

    chien

---
+++ DoggyDog/Doggy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoggyDog
+{
+    class Doggy
+    {
+        // Attribut privé du CHIEN DOGGY DOG
+        private string _nom;
+        private string _race;
+        private uint _age;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private bool _genre;
+        private bool _sterilise;
+        private bool _dresse;
+
+        public Doggy(string nom, string race, uint age, bool enOrdreDeVaccin, bool puce, bool genre, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+            _dresse = dresse;
+        }
+
+        public string AfficheCaracteristiques()
+        {
+            string chaine = "_nom" + _nom +
+
+            "race" + _race +
+            "age" + _age +
+            "enOrdreDeVaccin" + _enOrdreDeVaccin+
+            "puce" + _puce +
+            "genre" + _genre +
+            "sterilise" + _sterilise +
+            "dresse" + _dresse;
+            return chaine;
+        }
+
+
+
+    }
+}
---
+++ DoggyDog/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using DoggyDog;
+using System.Diagnostics;
+
+Console.WriteLine("premiere méthode");
+Doggy chien1 = new Doggy("Victor", "Berger allemand", 1, true, false, true,true,true,false);
+Doggy chien2 = new Doggy("", "", 2, );
+Doggy chien3 = new Doggy("", "", 3, );
+
+Console.WriteLine(chien1.AfficheCaracteristiques());
